7b5d4b7e342fce69c2cc97ee627a5a2e5b041764
subhajit735 <subhajit735@gmail.com>
2022-10-29 12:53:12 +0530

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -9,11 +9,20 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+  String city = "Guwahati";
+  String? error;
+  TextEditingController cityinp = TextEditingController();
   WeatherFactory wf = WeatherFactory("d5cd56e0736b66e2b79f53b941f03a68");
-  Future<Weather> fetchData() async {
-    Weather data = await wf.currentWeatherByCityName('Silchar');
-    print(data);
-    return data;
+  Future<Weather?> fetchData() async {
+    try {
+      Weather data = await wf.currentWeatherByCityName(city);
+
+      return data;
+    } catch (e) {
+      setState(() {
+        error = e.toString();
+      });
+    }
   }
 
   @override
@@ -23,20 +32,55 @@ class _homeState extends State<home> {
         title: Text("Weather App"),
         centerTitle: true,
       ),
-      body: FutureBuilder<Weather>(
-        future: fetchData(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          Weather weather = snapshot.data;
-          if (snapshot.hasData) {
-            return Center(
-              child: Column(children: [Text(weather.temperature.toString())]),
-            );
-          } else {
-            return Center(
-              child: Text("Getting your weather"),
-            );
-          }
-        },
+      body: Column(
+        children: [
+          TextField(
+            onSubmitted: (text) {
+              setState(() {
+                city = text;
+              });
+            },
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                labelText: 'Enter Name',
+                hintText: 'Enter Your Name'),
+          ),
+          FutureBuilder<Weather?>(
+            future: fetchData(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                Weather weather = snapshot.data;
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        weather.temperature.toString(),
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(weather.humidity.toString(),
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                      Text(weather.cloudiness.toString(),
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                );
+              } else {
+                return Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(error ?? 'Getting your weather...'),
+                    CircularProgressIndicator(),
+                  ],
+                ));
+              }
+            },
+          ),
+        ],
       ),
     );
   }
